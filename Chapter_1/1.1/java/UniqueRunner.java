702800b6c74a4dfb9e20c04215df05a9ec39820b
shumarvin <shumarvin@gmail.com>
2017-08-13 16:50:23 -0700

    code compiles, but doesn't output

--- Chapter_1/1.1/java/UniqueRunner.java
+++ Chapter_1/1.1/java/UniqueRunner.java
@@ -1,11 +1,39 @@
 import java.io.*;
 import java.lang.String;
+import java.util.Scanner;
 
 public class UniqueRunner
 {
-	public static void main(String args[])
+	//constants for input and output files
+	public static final String INPUT_FILE = "tests/test.txt";
+	public static final String OUTPUT_FILE = "tests/output.txt";
+
+	public static void main(String args[]) throws IOException
 	{
+		//Construct uld
 		UniqueLetterDeterminer uld = new UniqueLetterDeterminer();
+
+		try
+		{
+			//get input and output files
+			File in = new File(INPUT_FILE);
+			BufferedWriter out = new BufferedWriter(new FileWriter(OUTPUT_FILE));
+
+			//create scanner
+			Scanner scan = new Scanner(in);
+
+			//read from input file and write to output file
+			while (scan.hasNextLine())
+			{
+				uld.setWord(scan.nextLine());
+				out.write(uld.isUnique() + "");
+				out.newLine();
+			}
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 
 	}
 }
