0dc017c8942a23ce829a9688d2ab6085b710eee2
shumarvin <shumarvin@gmail.com>
2017-12-19 19:30:03 -0800

    problem done

--- Chapter_1/1.3/java/src/com/URLify/URLify.java
+++ Chapter_1/1.3/java/src/com/URLify/URLify.java
@@ -12,9 +12,43 @@ public class URLify
         trueLength = length;
     }
 
-    public void doURLify()
+
+    /**
+     * Replaces all spaces in arr with '%20' and does it in place
+     * Algorithm:
+     * 1. Find final length of arr by counting number of spaces,
+     * multiplying that by two (since a space is one char while '%20' is 3
+     * chars), and adding that to the true length. This will be the starting
+     * index to copy from
+     * 2. Go backwards starting at the last char. If it isn't a space, copy
+     * that char into its new place using the final length index. If it is,
+     * write '%20'.
+     * @return arr modified arr with the '%20''s in place
+     */
+    public char[] doURLify()
     {
-        
+        //find final length
+        int finalLengthIndex = trueLength + countSpaces() * 2;
+
+        //go backwards
+        for(int i = trueLength- 1; i >= 0; i--)
+        {
+            //if it's a space, replace it with '%20'
+            if(arr[i] == ' ')
+            {
+                arr[finalLengthIndex-3] = '%';
+                arr[finalLengthIndex-2] = '2';
+                arr[finalLengthIndex-1] = '0';
+                finalLengthIndex -=3;
+            }
+            //else copy the char
+            else
+            {
+                arr[finalLengthIndex-1] = arr[i];
+                finalLengthIndex--;
+            }
+        }
+        return arr;
     }
 
 
--- Chapter_1/1.3/java/tests/URLifyTester.java
+++ Chapter_1/1.3/java/tests/URLifyTester.java
@@ -1,8 +1,8 @@
 import com.URLify.URLify;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.URLify.URLify;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,5 +20,30 @@ public class URLifyTester
     {
         URLify test = new URLify("test two test   ",13);
         assertEquals(test.countSpaces(),2);
+    }
+    @Test
+    public void testCountSpaces0()
+    {
+        URLify test = new URLify("testzero   ",8);
+        assertEquals(test.countSpaces(),0);
+    }
+
+    @Test
+    public void testdoURLify1()
+    {
+        URLify test = new URLify("test one  ",8);
+        assertArrayEquals("test%20one".toCharArray(), test.doURLify());
+    }
+    @Test
+    public void testdoURLify2()
+    {
+        URLify test = new URLify("test two test    ",13);
+        assertArrayEquals("test%20two%20test".toCharArray(), test.doURLify());
+    }
+    @Test
+    public void testdoURLify0()
+    {
+        URLify test = new URLify("testzero",8);
+        assertArrayEquals("testzero".toCharArray(), test.doURLify());
     }
 }
