1e8aa041055386567b31271ce8a050737fc605ee
shumarvin <shumarvin@gmail.com>
2017-12-21 17:44:57 -0800

    removeDups working

--- Chapter2/2.1/java/src/RemoveDups.java
+++ Chapter2/2.1/java/src/RemoveDups.java
@@ -1,6 +1,5 @@
-import sun.awt.image.ImageWatched;
-
 import java.util.LinkedList;
+import java.util.Hashtable;
 
 public class RemoveDups<T>
 {
@@ -11,10 +10,31 @@ public class RemoveDups<T>
     {
         this.list = list;
     }
-
+    /*
+        Removes all duplicates in list
+        Algorithm:
+        1. Create empty hashtable to keep track of dups
+        2. Go through linked list. If the current Node is
+            in the hashtable, delete it. If not, then add
+            it to the hashtable.
+        @return list the linkedlist without any duplicates
+     */
     public LinkedList<T> removeDups()
+    {
+        Hashtable<T, Boolean> hashTable = new Hashtable<>();
+        list.forEach((currentNode)->
+        {
+            if(hashTable.contains((currentNode)))
+                hashTable.remove((currentNode));
+            else
+                hashTable.put(currentNode,true);
+        });
+        return list;
+    }
+
+    public LinkedList<T> removeDups2()
     {
         return null;
     }
-
 }
+
--- Chapter2/2.1/java/tests/RemoveDupsTester.java
+++ Chapter2/2.1/java/tests/RemoveDupsTester.java
@@ -1,14 +1,13 @@
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class RemoveDupsTester
 {
     @Test
-    public void testRemoveDups()
+    public void testRemoveDupsInteger()
     {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
@@ -21,5 +20,118 @@ public class RemoveDupsTester
         list.remove(1);
         assertEquals(list, test.removeDups());
     }
+    @Test
+    public void testRemoveDups2Integer()
+    {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(27);
+        list.add(30);
+        list.add(27);
+        list.add(15);
 
+        RemoveDups<Integer> test = new RemoveDups<>(list);
+        list.remove(1);
+        assertEquals(list, test.removeDups2());
+    }
+    @Test
+    public void testRemoveDupsIntegerNoDups()
+    {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(27);
+        list.add(30);
+        list.add(15);
+
+        RemoveDups<Integer> test = new RemoveDups<>(list);
+        assertEquals(list, test.removeDups());
+    }
+    @Test
+    public void testRemoveDups2IntegerNoDups()
+    {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(27);
+        list.add(30);
+        list.add(15);
+
+        RemoveDups<Integer> test = new RemoveDups<>(list);
+        assertEquals(list, test.removeDups2());
+    }
+    @Test
+    public void testRemoveDupsArrayList()
+    {
+        ArrayList<Double> one = new ArrayList<>();
+        one.add(2.5);
+        ArrayList<Double> oneClone = new ArrayList<>();
+        oneClone.add(2.5);
+        ArrayList<Double> two = new ArrayList<>();
+        two.add(3.9);
+        two.add(100.0);
+
+        LinkedList<ArrayList<Double>> list = new LinkedList<>();
+        list.add(new ArrayList<>());
+        list.add(one);
+        list.add(oneClone);
+        list.add(two);
+
+        RemoveDups<ArrayList<Double>> test = new RemoveDups<>(list);
+        list.remove(2);
+        assertEquals(list, test.removeDups());
+    }
+    @Test
+    public void testRemoveDups2ArrayList()
+    {
+        ArrayList<Double> one = new ArrayList<>();
+        one.add(2.5);
+        ArrayList<Double> oneClone = new ArrayList<>();
+        oneClone.add(2.5);
+        ArrayList<Double> two = new ArrayList<>();
+        two.add(3.9);
+        two.add(100.0);
+
+        LinkedList<ArrayList<Double>> list = new LinkedList<>();
+        list.add(new ArrayList<>());
+        list.add(one);
+        list.add(oneClone);
+        list.add(two);
+
+        RemoveDups<ArrayList<Double>> test = new RemoveDups<>(list);
+        list.remove(2);
+        assertEquals(list, test.removeDups2());
+    }
+    @Test
+    public void testRemoveDupsArrayListNoDups()
+    {
+        ArrayList<Double> one = new ArrayList<>();
+        one.add(2.5);
+        ArrayList<Double> two = new ArrayList<>();
+        two.add(3.9);
+        two.add(100.0);
+
+        LinkedList<ArrayList<Double>> list = new LinkedList<>();
+        list.add(new ArrayList<>());
+        list.add(one);
+        list.add(two);
+
+        RemoveDups<ArrayList<Double>> test = new RemoveDups<>(list);
+        assertEquals(list, test.removeDups());
+    }
+    @Test
+    public void testRemoveDups2ArrayListNoDups()
+    {
+        ArrayList<Double> one = new ArrayList<>();
+        one.add(2.5);
+        ArrayList<Double> two = new ArrayList<>();
+        two.add(3.9);
+        two.add(100.0);
+
+        LinkedList<ArrayList<Double>> list = new LinkedList<>();
+        list.add(new ArrayList<>());
+        list.add(one);
+        list.add(two);
+
+        RemoveDups<ArrayList<Double>> test = new RemoveDups<>(list);
+        assertEquals(list, test.removeDups2());
+    }
 }
