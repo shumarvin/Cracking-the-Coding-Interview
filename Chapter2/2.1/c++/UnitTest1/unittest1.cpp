e9015c765c90492386e5bb95688f69e705306dea
shumarvin <shumarvin@gmail.com>
2017-12-22 16:24:29 -0800

    removeDups working

--- Chapter2/2.1/c++/UnitTest1/unittest1.cpp
+++ Chapter2/2.1/c++/UnitTest1/unittest1.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../removeDups.cpp"
 #include <list>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -21,12 +22,59 @@ namespace UnitTest1
 			list.push_back(27);
 			list.push_back(15);
 			std::list<int> actual(list);
-			removeDups<int>(list);
+			removeDups(list);
 			it = actual.begin();
 			advance(it, 3);
 			actual.erase(it);
 			Assert::AreEqual(true, (std::equal(std::begin(list),std::end(list),std::begin(actual))));
 		}
+		TEST_METHOD(removeDups0)
+		{
+			std::list<int> list;
+			list.push_back(1);
+			list.push_back(27);
+			list.push_back(30);
+			list.push_back(15);
+			std::list<int> actual(list);
+			removeDups(list);
+			Assert::AreEqual(true, (std::equal(std::begin(list), std::end(list), std::begin(actual))));
+		}
 
+		TEST_METHOD(removeDupsVector2)
+		{
+			std::list<std::vector<int>> list;
+			std::list<std::vector<int>>::iterator actualit;
+			std::vector<int> ones(3, 5);
+			std::vector<int> twos(2, 7);
+			std::vector<int> onecopy(3, 5);
+			std::vector<int> threes(3, 8);
+			std::vector<int> twocopy(twos);
+			list.push_back(ones);
+			list.push_back(twos);
+			list.push_back(onecopy);
+			list.push_back(threes);
+			list.push_back(twocopy);
+			std::list<std::vector<int>> actual(list);
+			actualit = actual.begin();
+			advance(actualit, 2);
+			actual.erase(actualit);
+			actual.pop_back();
+			removeDups(list);
+			Assert::AreEqual(true, (std::equal(std::begin(list), std::end(list), std::begin(actual))));
+		}
+		TEST_METHOD(removeDupsVector0)
+		{
+			std::list<std::vector<int>> list;
+			std::list<std::vector<int>>::iterator actualit;
+			std::vector<int> ones(3, 5);
+			std::vector<int> twos(2, 7);			
+			std::vector<int> threes(3, 8);		
+			list.push_back(ones);
+			list.push_back(twos);		
+			list.push_back(threes);
+			std::list<std::vector<int>> actual(list);
+			removeDups(list);
+			Assert::AreEqual(true, (std::equal(std::begin(list), std::end(list), std::begin(actual))));
+		}
 	};
 }
--- Chapter2/2.1/c++/removeDups.cpp
+++ Chapter2/2.1/c++/removeDups.cpp
@@ -1,7 +1,21 @@
 #include <list>
+#include <map>
 
 template <typename T>
 void removeDups(typename std::list<T> &lst)
 {
+	typename std::map<T, bool> elements;
+	typename std::list<T>::iterator iterator = lst.begin();
+	while(iterator != lst.end())
+	{
+		if (elements.find(*iterator) != elements.end())
+			iterator = lst.erase(iterator);
+			
+		else
+		{
+			elements.insert(typename std::pair<T,bool>(*iterator, true));
+			iterator++;
+		}
+	}	
 	return;
 }
