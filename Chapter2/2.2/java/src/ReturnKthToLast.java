134f5ef2d9f485075a818a012c362d50c6a6ee3f
shumarvin <shumarvin@gmail.com>
2017-12-23 17:59:03 -0800

    initial commit for 2.2 java version

---
+++ Chapter2/2.2/java/src/ReturnKthToLast.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class ReturnKthToLast<T>
+{
+    private LinkedList<T> list;
+
+    public ReturnKthToLast(LinkedList<T> list)
+    {
+        this.list = list;
+    }
+
+    public T returnKthToLast(int k)
+    {
+        return null;
+    }
+
+}
---
+++ Chapter2/2.2/java/test/ReturnKthToLastTester.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ReturnKthToLastTester
+{
+    @Test
+    public void testReturn1stToLast()
+    {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,77,45,90,80));
+        ReturnKthToLast<Integer> test = new ReturnKthToLast<>(list);
+        assertEquals(90, (int)test.returnKthToLast(1));
+    }
+    @Test
+    public void testReturn2ndToLast()
+    {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,77,45,90,80));
+        ReturnKthToLast<Integer> test = new ReturnKthToLast<>(list);
+        assertEquals(45, (int)test.returnKthToLast(2));
+    }
+    @Test
+    public void testReturn0thToLast()
+    {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,77,45,90,80));
+        ReturnKthToLast<Integer> test = new ReturnKthToLast<>(list);
+        assertEquals(80, (int)test.returnKthToLast(0));
+    }
+    @Test
+    public void testReturn1stElement()
+    {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,77,45,90,80));
+        ReturnKthToLast<Integer> test = new ReturnKthToLast<>(list);
+        assertEquals(1, (int)test.returnKthToLast(4));
+    }
+}
