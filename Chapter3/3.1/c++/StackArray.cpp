12b8d9023e4ba4ce4b20b53d71c85263f6c389cc
shumarvin <shumarvin@gmail.com>
2017-12-29 20:04:17 -0800

    3.1 c++ version push working

--- Chapter3/3.1/c++/StackArray.cpp
+++ Chapter3/3.1/c++/StackArray.cpp
@@ -24,7 +24,37 @@ StackArray::StackArray(int size)
 	@throws FullStackException if specified stack is
 	already full.
 */
-void StackArray::push(int num, int numstack)
+void StackArray::push(int num, int numStack)
 {
+	//get head index of requested stack
+	int stackIndex = stackHeads[numStack - 1];
 
+	//make sure stack isn't already full
+	if (stackIndex == numStack * stackSize - 1)
+		throw FullStackException();
+
+	/*
+	Push onto stack.
+	If stack is empty, insert num at first
+	spot on stack and update stackHeads
+	accordingly
+	*/
+	if (stackIndex == -1)
+	{
+		/*
+		Index for first spot for stack 1 is 0, 2nd stack is 5,
+		3rd spot is 10, so formula is:
+		(numStack-1) * stackSize
+		*/
+		stackIndex = (numStack - 1) * stackSize;
+		stackArray[stackIndex] = num;
+		stackHeads[numStack - 1] = stackIndex;
+	}
+	//else, insert at one above current head index
+	//and update stack head index
+	else
+	{
+		stackArray[stackIndex + 1] = num;
+		stackHeads[numStack - 1]++;
+	}
 }
